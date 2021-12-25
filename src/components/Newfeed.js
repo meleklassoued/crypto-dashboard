47bec6228bff85bae78c9425e7a7ee7ada10e1b7
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-12-25 17:41:34 +0100

    complete the logic of newfeed

--- src/components/Newfeed.js
+++ src/components/Newfeed.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Newfeed() {
-    return (
-        <div className='news-feed'>
-            beslema
-        </div>
-    )
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    var options = {
+      method: "GET",
+      url: "https://crypto-news15.p.rapidapi.com/news",
+      headers: {
+        "x-rapidapi-host": "crypto-news15.p.rapidapi.com",
+        "x-rapidapi-key": "50dfe8bbe6msha025a531747029dp119647jsn255a560e8e36",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setArticles(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+  //   console.log(articles);
+  const displayedArticle = articles?.slice(0, 10);
+
+  return (
+    <div className='news-feed'>
+      <h2>News feed</h2>
+      <div>
+        {displayedArticle?.map((article) => (
+          <p>{article.title}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Newfeed
+export default Newfeed;
